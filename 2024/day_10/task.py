6505e33c20f490e4c71ec9a1c0ffb63f576a3db1
Grognak88 <25593779+Grognak88@users.noreply.github.com>
2024-12-10 08:47:51 +0000

    fjernet threading :( ble raskere da

--- 2024/day_10/task.py
+++ 2024/day_10/task.py
@@ -1,4 +1,5 @@
-from threading import Thread
+from time import time
+start_time = time()
 
 class TreeNode:
     def __init__(self, data):
@@ -19,7 +20,6 @@ threads = []
 starting_trees = {key: TreeNode(key) for key, val in data.items() if val == 0}
 
 def traverse(origin, current_location, incline):
-    global threads
     directions = (current_location + (0-1j), current_location + (0+1j), current_location + (-1+0j), current_location + (1+0j))
     if incline == 9:
         return
@@ -27,27 +27,23 @@ def traverse(origin, current_location, incline):
     for next_step in directions:        
         if next_step in data and data[next_step] == incline + 1:
             starting_locations[origin].add((next_step, data[next_step]))
-            thread = Thread( target=traverse, args=(origin, next_step, incline + 1))
-            thread.start()   
-            threads.append(thread)
+            traverse(origin, next_step, incline+1)
+            
 
 def traverse_tree(current_location: TreeNode, incline: int):
-    global threads  
+ 
 
     if incline == 9:
         return
-    if current_location.data == (1+6j):
-        pass
     next_incline = incline +1
     directions = (current_location.data + (0-1j), current_location.data + (0+1j), current_location.data + (-1+0j), current_location.data + (1+0j))    
 
     for next_step in directions:
         if next_step in data and data[next_step] == next_incline:
             next_node = TreeNode(next_step)
-            current_location.add_child(next_node)            
-            thread = Thread( target=traverse_tree, args=(next_node, next_incline))
-            thread.start()   
-            threads.append(thread)   
+            current_location.add_child(next_node)  
+            traverse_tree(next_node, next_incline)        
+             
 
 for origin in starting_locations:
     traverse(origin,origin,0)
@@ -67,7 +63,9 @@ for start, paths in starting_locations.items():
                 score += 1
     scores.append(score)
 
-print(sum(scores))
+print(f"{sum(scores)}: ({time() - start_time:.3f}s)")
+
+split_time = time()
 
 for origin, root in starting_trees.items():
     traverse_tree(root,0)
@@ -87,4 +85,4 @@ def walk_tree(node: TreeNode, ):
 for root in starting_trees.values():
     scores_2.append(walk_tree(root))
 
-print(sum(scores_2))
+print(f"{sum(scores_2)}: ({time() - split_time:.3f}s)")
