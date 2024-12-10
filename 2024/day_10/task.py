5842742533a09a8bb955603a69219893bad513f4
Grognak88 <25593779+Grognak88@users.noreply.github.com>
2024-12-10 11:14:02 +0000

    fjernet print

--- 2024/day_10/task.py
+++ 2024/day_10/task.py
@@ -38,8 +38,7 @@ def workers(start, step, dimension):
 
 def main_thread():
     step = 3
-    for i in range(0,dimension, 3):
-        print(i)
+    for i in range(0,dimension, 3):       
         thread = Thread(target=workers, args=(i, step, dimension))
         thread.start()
         thread_pool.append(thread)
