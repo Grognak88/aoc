2cc343c51711fcebc1383d8d91cf56ef8788d2f6
Grognak88 <25593779+Grognak88@users.noreply.github.com>
2024-12-10 09:04:02 +0000

    Refactored to only use tree

--- 2024/day_10/task.py
+++ 2024/day_10/task.py
@@ -12,67 +12,38 @@ with open("/home/onyxia/work/aoc/2024/day_10/input.txt") as file:
     data = file.read().split("\n")
     data = [list(line) for line in data]
     data = {j+i*1j: int(data[i][j]) for i in range(len(data)) for j in range(len(data[0]))}
-    
-starting_locations = {key: set() for key, val in data.items() if val == 0}
-
-starting_trees = {key: TreeNode(key) for key, val in data.items() if val == 0}
-
-def traverse(origin, current_location, incline):
-    directions = (current_location + (0-1j), current_location + (0+1j), current_location + (-1+0j), current_location + (1+0j))
-    if incline == 9:
-        return
-    
-    for next_step in directions:        
-        if next_step in data and data[next_step] == incline + 1:
-            starting_locations[origin].add((next_step, data[next_step]))
-            traverse(origin, next_step, incline+1)
-            
-
+   
+roots = {key: TreeNode((key, val)) for key, val in data.items() if val == 0}
+           
 def traverse_tree(current_location: TreeNode, incline: int):
- 
-
     if incline == 9:
         return
     next_incline = incline +1
-    directions = (current_location.data + (0-1j), current_location.data + (0+1j), current_location.data + (-1+0j), current_location.data + (1+0j))    
+    directions = (current_location.data[0] + (0-1j), current_location.data[0] + (0+1j), current_location.data[0] + (-1+0j), current_location.data[0] + (1+0j))    
 
     for next_step in directions:
         if next_step in data and data[next_step] == next_incline:
-            next_node = TreeNode(next_step)
+            next_node = TreeNode((next_step, next_incline))
             current_location.add_child(next_node)  
             traverse_tree(next_node, next_incline)        
              
-
-for origin in starting_locations:
-    traverse(origin,origin,0)
-
-scores = []
-
-for start, paths in starting_locations.items():
-    score = 0
-    for location in paths:
-        match location:
-            case (_, 9):
-                score += 1
-    scores.append(score)
-
-print(f"{sum(scores)}: ({time() - start_time:.3f}s)")
-
-split_time = time()
-
-for origin, root in starting_trees.items():
+for origin, root in roots.items():
     traverse_tree(root,0)
 
+scores = []
 scores_2 = []
 
-def walk_tree(node: TreeNode, ):
-    if len(node.children) == 0 and data[node.data] == 9:
+def walk_tree(node: TreeNode, unique_nodes):
+    if len(node.children) == 0 and node.data[1] == 9:
+        unique_nodes.add(node.data)
         return 1
 
-    return sum((walk_tree(child) for child in node.children))
+    return sum((walk_tree(child, unique_nodes) for child in node.children))
 
+for root in roots.values():
+    unique_nodes = set()
+    scores_2.append(walk_tree(root,unique_nodes))
+    scores.append(len(unique_nodes))
 
-for root in starting_trees.values():
-    scores_2.append(walk_tree(root))
-
-print(f"{sum(scores_2)}: ({time() - split_time:.3f}s)")
+print(f"{sum(scores)}: ({time() - start_time:.3f}s)")
+print(f"{sum(scores_2)}: ({time() - start_time:.3f}s)")
