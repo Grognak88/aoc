3e6561e10e932f60eda0626e102a7aad4b1f22c7
Grognak88 <25593779+Grognak88@users.noreply.github.com>
2024-12-11 11:57:13 +0000

    slight improvement

--- 2024/day_11/task.py
+++ 2024/day_11/task.py
@@ -1,8 +1,29 @@
 from functools import cache
+from time import time
+
+start_time = time()
 
 with open("input.txt") as file:
   data = file.read().replace("\n", "").split(" ")
   data = list(map(int, data))
+
+@cache
+def digits(num):
+  count=0
+  while(num>0):
+    count=count+1
+    num=num//10
+  return count
+
+@cache
+def tenbase(zeros):
+  val = 1
+  i = 0
+  while i < zeros:
+    val *= 10
+    i += 1
+  
+  return val
 
 @cache
 def count(num, i, max_depth):   
@@ -12,9 +33,10 @@ def count(num, i, max_depth):
   if num == 0:        
     return count(1, i+1, max_depth)
   elif len(str(num)) % 2 == 0:
-    length = int(len(str(num)) // 2)
-    part1 = int(str(num)[:length])
-    part2 = int(str(num)[length:])             
+    length = digits(num)//2
+    splitter = tenbase(length)
+    part1 = num // splitter
+    part2 = num % splitter            
     return count(part1, i+1, max_depth) + count(part2, i+1, max_depth)
   else:
     new_val = int(num * 2024)
@@ -26,9 +48,9 @@ count_2 = 0
 for start in data:
   count_1 += count(start, 0, 25)
 
-print(count_1)
+print(f"{count_1}: {time() - start_time:.3f}s")
 
 for loc in data:
   count_2 += count(loc, 0, 75)
 
-print(count_2)
+print(f"{count_2}: {time() - start_time:.3f}s")
