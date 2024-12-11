0cb7f07b0f56343eaa451e20b93bb2106779b17b
Grognak88 <25593779+Grognak88@users.noreply.github.com>
2024-12-11 11:05:27 +0000

    fix for part 2

--- 2024/day_11/task.py
+++ 2024/day_11/task.py
@@ -1,46 +1,34 @@
-class TreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.children = []
-    def add_child(self, child):
-        self.children.append(child)
-
+from functools import cache
 
 with open("input.txt") as file:
-    data = file.read().replace("\n", "").split(" ")
-    data = {i: TreeNode(int(val)) for i, val in enumerate(data)}
+  data = file.read().replace("\n", "").split(" ")
+  data = list(map(int, data))
 
-nums_1 = []
-nums_2 = []
+@cache
+def count(num, i, max_depth):   
+  if i == max_depth:      
+    return 1
 
-def grow_tree(node: TreeNode, i, max_depth):
-    if node.data == 0:
-        new_child = TreeNode(1)
-        node.add_child(new_child)
-    elif len(str(node.data)) % 2 == 0:
-        length = len(str(node.data)) // 2
-        part1 = TreeNode(int(str(node.data)[:length]))
-        part2 = TreeNode(int(str(node.data)[length:]))        
-        node.add_child(part1)
-        node.add_child(part2)
-    else:
-        new_val = TreeNode(node.data * 2024)
-        node.add_child(new_val)
+  if num == 0:        
+    return count(1, i+1, max_depth)
+  elif len(str(num)) % 2 == 0:
+    length = int(len(str(num)) // 2)
+    part1 = int(str(num)[:length])
+    part2 = int(str(num)[length:])             
+    return count(part1, i+1, max_depth) + count(part2, i+1, max_depth)
+  else:
+    new_val = int(num * 2024)
+    return count(new_val, i+1, max_depth)
+     
+count_1 = 0
+count_2 = 0
 
-    if i+1 == 25:
-        nums_1.append(node.children)
-    
-    if i+1 == max_depth:
-        nums_2.append(node.children)
-        return
+for start in data:
+  count_1 += count(start, 0, 25)
 
-    for child in node.children:
-        grow_tree(child, i+1, max_depth)    
+print(count_1)
 
-for root in data.values():
-    grow_tree(root, 0, 75)
+for loc in data:
+  count_2 += count(loc, 0, 75)
 
-print(len([d for ld in nums_1 for d in ld]))
-
-print(len([d for ld in nums_2 for d in ld]))
-
+print(count_2)
